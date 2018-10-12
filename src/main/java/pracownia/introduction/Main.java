d68185a3bb0b678e0c06a095ca218c02dcfe4d10
marcinljablonski <marcinljablonski@gmail.com>
2018-10-12 15:09:52 +0200

    added logger

--- src/main/java/pracownia/introduction/Main.java
+++ src/main/java/pracownia/introduction/Main.java
@@ -1,8 +1,11 @@
 package pracownia.introduction;
 
-public class Main {
+import org.apache.log4j.Logger;
 
+public class Main {
+    static Logger log = Logger.getLogger("name");
     public static void main(String [ ] args) {
         System.out.println("Hello World");
+        log.info("message");
     }
 }
